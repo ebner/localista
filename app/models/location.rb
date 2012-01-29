8f517dd323a39d7c6ce708f0d44c50e71b744081
Hannes Ebner <hannes.ebner@gmail.com>
2012-01-29 12:41:54 +0000

    fixed gmaps problem

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   belongs_to :user
-  acts_as_gmappable :lat => "lat", :lng => "long", :validation => true
+  acts_as_gmappable :lat => "lat", :lng => "long", :validation => true, :process_geocoding => false
   
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
@@ -10,7 +10,6 @@ class Location < ActiveRecord::Base
   def gmaps4rails_infowindow
     the_user = User.find(self.user_id)
     text = "<h4>#{the_user.name} has available:</h4>"
-    text += "<h4>#{the_user.location.lat} #{the_user.location.long} </h4>"
     if the_user.items != nil
       the_user.items.each do |i|
         text += "<p><a href=/items/#{i.id}>#{i.name}</a></p>"
