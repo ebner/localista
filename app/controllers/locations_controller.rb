5f8566de94f634c5e03e074635a696fed1f06051
Jorge Zapico <zapico@kth.se>
2012-01-29 11:10:16 +0100

    Test

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,29 +1,31 @@
 class LocationsController < ApplicationController
 
  def show
-   @json = Location.all.to_gmaps4rails do |location, marker|
-    # marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
-     case location.user.category
-     when "fish" 
-       marker.picture({:picture => "/icons/fish.png",:width   => "50",:height  => "50"}) 
-     when "fruit" 
-       marker.picture({:picture => "/icons/apple.png",:width   => "50",:height  => "50"}) 
-     when "meat" 
-       marker.picture({:picture => "/icons/meat.png",:width   => "50",:height  => "50"}) 
-     when "grains" 
-       marker.picture({:picture => "/icons/grains.png",:width   => "50",:height  => "50"})       
-     when "game" 
-       marker.picture({:picture => "/icons/moose.png",:width   => "50",:height  => "50"}) 
-     when "dairy" 
-       marker.picture({:picture => "/icons/dairy.png",:width   => "50",:height  => "50"}) 
-     else
-       marker.picture({:picture => "/icons/farm.png",:width   => "50",:height  => "50"})
-     end
-     
-     #marker.title   "i'm the title"
-     #marker.sidebar "i'm the sidebar"
-     marker.json({ :id => location.id, :foo => "bar" })
-   end
+   @json = Location.all.to_gmaps4rails
+   / do |location, marker|
+     # marker.infowindow render_to_string(:partial => "/users/my_template", :locals => { :object => user})
+      case location.user.category
+      when "fish" 
+        marker.picture({:picture => "/icons/fish.png",:width   => "50",:height  => "50"}) 
+      when "fruit" 
+        marker.picture({:picture => "/icons/apple.png",:width   => "50",:height  => "50"}) 
+      when "meat" 
+        marker.picture({:picture => "/icons/meat.png",:width   => "50",:height  => "50"}) 
+      when "grains" 
+        marker.picture({:picture => "/icons/grains.png",:width   => "50",:height  => "50"})       
+      when "game" 
+        marker.picture({:picture => "/icons/moose.png",:width   => "50",:height  => "50"}) 
+      when "dairy" 
+        marker.picture({:picture => "/icons/dairy.png",:width   => "50",:height  => "50"}) 
+      else
+        marker.picture({:picture => "/icons/farm.png",:width   => "50",:height  => "50"})
+      end
+
+      #marker.title   "i'm the title"
+      #marker.sidebar "i'm the sidebar"
+      marker.json({ :id => location.id, :foo => "bar" })
+    end /
+    
  end
  
 end
